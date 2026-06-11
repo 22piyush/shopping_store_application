5b6ceb9d515a79eaa522a8b390054d3529a884af
22piyush <piyushaglawe05@gmail.com>
2026-06-11 22:34:35 +0530

    done

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { assets } from "../assets/assets";
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <div className="flex items-center justify-between py-5 font-medium">
+      <img src={assets.logo} className="w-36" />
+
+      <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
+        <NavLink className="flex flex-col items-center gap-1">
+
+        </NavLink>
+      </ul>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
