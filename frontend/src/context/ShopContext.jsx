d9e5f1cba48f87ee12b15b6caa58ec2429d38d9d
22piyush <piyushaglawe05@gmail.com>
2026-06-12 23:22:11 +0530

    done

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -4,10 +4,14 @@ import { products } from "../assets/assets";
 export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
+    
   const currency = "$";
+  const delivery_fee = 10;
 
   const value = {
     products,
+    currency,
+    delivery_fee,
   };
 
   return (
